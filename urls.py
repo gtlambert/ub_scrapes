6381b6fd8d83902f1fe6c1386404cf26eda364bf
George Lambert <georgethomaslambert@gmail.com>
2016-04-14 14:35:48 +0100

    update

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import scrapes.views as scrapes_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', scrapes_views.home, name='home'),
 ]
